bcfe6bf00c90e4d4afb70c061b5dd8a4addbfec2
MIchaelDoan <michael.doan18@gmail.com>
2025-02-03 16:10:43 +1100

    Start of 4.1

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = 'mongodb+srv://michaeldoan:H_Nhx44qsKj77Qi@cluster0.u0i8w.mongodb.net/BlogApp?retryWrites=true&w=majority'
+
+mongoose.connect(mongoUrl)
+
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+      console.log("Hello");
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+      console.log("Hello");
+    })
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
