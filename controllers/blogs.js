a33feb7a95ab2a589fbe7446f830ba3bf2f39285
MIchaelDoan <michael.doan18@gmail.com>
2025-02-25 16:16:20 +1100

    Finished chapter 4

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,53 +1,80 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "");
+  }
+  return null;
+};
 
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", {
+    username: 1,
+    name: 1,
+    id: 1,
+  });
   response.json(blogs);
 });
 
 blogsRouter.get("/:id", async (request, response) => {
   const blog = await Blog.findById(request.params.id);
-  if(blog){
+  if (blog) {
     response.json(blog);
-  }else{
+  } else {
     response.status(404).end();
   }
-})
+});
 
 blogsRouter.post("/", async (request, response) => {
-  const blog = new Blog(request.body);
+  const body = request.body;
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token invalid" });
+  }
+  const user = await User.findById(decodedToken.id);
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user.id,
+  });
   if (!request.body.title || !request.body.url) {
     response.status(400).end();
   } else {
     const savedBlog = await blog.save();
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
     response.status(201).json(savedBlog);
   }
 });
 
-blogsRouter.delete("/:id", async (request, response) =>{
-  await Blog.findByIdAndDelete(request.params.id)
+blogsRouter.delete("/:id", async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id);
   response.status(204).end();
+});
 
-})
-
-blogsRouter.put("/:id", async (request, response) =>{
+blogsRouter.put("/:id", async (request, response) => {
   const body = request.body;
   const newBlog = {
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes
-  }
+    likes: body.likes,
+  };
 
-  await Blog.findByIdAndUpdate(request.params.id, newBlog, {new: true})
-    .then((updatedBlog)=>{
+  await Blog.findByIdAndUpdate(request.params.id, newBlog, { new: true })
+    .then((updatedBlog) => {
       response.status(200).json(updatedBlog);
     })
     .catch((error) => {
-      console.log(error)
-    })
-
-})
+      console.log(error);
+    });
+});
 
 module.exports = blogsRouter;
--- models/blog.js
+++ models/blog.js
@@ -1,28 +1,32 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const blogSchema = new mongoose.Schema({
-    title: {
-      type: String,
-      required: true,
-    },
-    author: String,
-    url: {
-      type: String,
-      required: true,
-    },
-    likes: Number
-  })
+  title: {
+    type: String,
+    required: true,
+  },
+  author: String,
+  url: {
+    type: String,
+    required: true,
+  },
+  likes: Number,
 
-  blogSchema.set("toJSON", {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString();
-      if(!returnedObject.likes){
-        returnedObject.likes = 0;
-      }
-      delete returnedObject._id;
-      delete returnedObject.__v;
-    },
-  });
-  
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
 
-module.exports = mongoose.model('Blog', blogSchema);
+blogSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    if (!returnedObject.likes) {
+      returnedObject.likes = 0;
+    }
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Blog", blogSchema);
